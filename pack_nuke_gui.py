12fbe6d63e42ecf4ea86da53dc68dfa7c6b671ab
jrsndlr <jrsndlr@gmail.com>
2024-08-09 15:08:37 +0200

    GUI fixes

--- pack_nuke_gui.py
+++ pack_nuke_gui.py
@@ -1,5 +1,7 @@
 # This Python file uses the following encoding: utf-8
 
+import csv
+import datetime
 from functools import partial
 import json
 import logging
@@ -17,6 +19,7 @@ from ui_pack_nuke import *
 from ui_images_rc import *
 
 import pprint
+
 
 class TreeItem:
     """A Json item corresponding to a line in QTreeView"""
@@ -312,16 +315,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
     def __init__(self, parent = None):
         super().__init__()
 
+        self.place_list = None
         self.ui = Ui_pack_gui()
         self.ui.setupUi(self)
         ui_images_rc.qInitResources()
-        self.resize(1400, 300)
+        #self.resize(1400, 300)
 
         # data structs
         self.found_versions = None
         self.filtered_versions = None
 
-        #
+        # from the gui
         self.search_root = None
         self.search_contains = None
         self.search_for = None
@@ -332,11 +336,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
         self.versions_get_last = None
         self.versions_last = None
         self.settings_file = None
-        self.settings_json = None
+        self.settings_json = {}
         self.place_source_default = None
 
-        self.column_titles = ['Path', 'Open', 'Select']
+        self.column_titles = ['Path', 'Tokens', 'Open', 'Select']
         self.user_stopped = False
+        self.place_list = []
+        self.place_source = ''
+        self.place_target = ''
+        self.anatomy = {}
+
+
 
         if getattr(sys, 'frozen', False):
             # If the application is run as a bundle, the PyInstaller bootloader
@@ -350,7 +360,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
                 os.path.abspath(__file__)).replace('\\', '/')
 
         # settings
-        prefs_pth = self.application_path + '\\settings.ini'
+        prefs_pth = self.application_path + '/settings.ini'
         print('-> init prefs')
         print('-> prefs: ' + prefs_pth)
         self.settings = QtCore.QSettings(prefs_pth, QtCore.QSettings.IniFormat)
@@ -366,9 +376,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
             except:
                 print('-> Failed to write preferences.')
 
-
-
-        # -------------------------------------------------------------------
         # signals
         # path
         self.ui.search_root.textChanged.connect(self.search_changed)
@@ -382,10 +389,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
         self.ui.versions_get_last.clicked.connect(self.versions_changed)
         self.ui.versions_first.valueChanged.connect(self.versions_changed)
         self.ui.versions_last.valueChanged.connect(self.versions_changed)
-        # all
+        # mark all/none
         self.ui.mark_all.clicked.connect(self.select_all)
         self.ui.mark_none.clicked.connect(self.select_none)
 
+        # places change
+        self.ui.place_source.currentIndexChanged.connect(self.place_change)
+        self.ui.place_target.currentIndexChanged.connect(self.place_change)
+
+        # job name
+        self.ui.job_name.textChanged.connect(self._validate_job_name)
+
+        # job folder browse
+        self.ui.job_folder_browse.clicked.connect(self.get_job_folder)
+
+        # settings json path
         self.ui.settings_file.textChanged.connect(self.display_json)
 
         # go
@@ -396,11 +414,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
         self.stop_short = QtGui.QShortcut(QtGui.QKeySequence('Ctrl+.'), self)
         self.stop_short.activated.connect(self.user_stop)
 
-        logging.basicConfig(
-            filename=os.path.join(self.application_path, 'pack_nuke_gui.log')
-            .replace('\\', '/'), level=logging.DEBUG)
-        logging.info('Started at: ' + time.strftime("%Y-%m-%d, %H:%M"))
-
+        # show settings json
         self.model = JsonModel()
         self.ui.settings.setModel(self.model)
         self.display_json()
@@ -501,6 +515,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
         self.ui.search_root.setText(fldr)
         self.search_changed()
 
+    def get_job_folder(self):
+         file_name = QtWidgets.QFileDialog.getExistingDirectory(
+             self,
+             "Select Job Folder",
+             str(self.ui.job_folder.displayText())
+         )
+         self.ui.job_folder.setText(os.path.realpath(file_name).replace('\\', '/'))
+
     def _get_search_gui(self):
         self.search_root = str(self.ui.search_root.displayText()).replace('\\', '/')
         self.search_contains = str(self.ui.search_contains.displayText()).replace('\\', '/')
@@ -572,6 +594,64 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
             lst[k] = vs
         self.found_versions = lst
 
+
+    def match_regex(self, filter, source, ):
+        """
+        Checks if the filter matches the source by regex
+        """
+        result = None
+        try:
+            _re = re.compile(filter['search'])
+        except:
+            return None
+        if filter['replace'] and filter['replace'] != '':
+            try:
+                result = _re.match(source).expand(filter['replace'])
+            except:
+                return None
+        else:
+            m = re.search(filter['search'], source)
+            if m is not None:
+                try:
+                    result = m.group(1)
+                except:
+                    result = m.group(0)
+
+        if result is not None:
+            if filter['lowercase']:
+                result = result.lower()
+            if filter['uppercase']:
+                result = result.upper()
+
+        return result
+
+
+    def get_script_path_tags(self, full_path):
+        if self.settings_json is None:
+            return {}
+        place_source = self.ui.place_source.currentText()
+        if not self.settings_json['places'][place_source]['use_script_path_tags']:
+            return {}
+        tags_list = self.settings_json['places'][place_source]['script_path_tags']
+        if tags_list is None or len(tags_list) == 0:
+            return {}
+
+        full_path = full_path.replace('\\', '/')
+        file_name = os.path.basename(full_path)
+
+        tokens = {}
+        for one_filter in tags_list:
+            match = None
+            if one_filter['source'] == 'File Name':
+                match = self.match_regex(one_filter, source=file_name)
+            elif one_filter['source'] == 'File Path':
+                match = self.match_regex(one_filter, source=full_path)
+
+            if match is not None:
+                tokens[one_filter['name']] = match
+        return tokens
+
+
     def filter_versions(self):
         """
         This assumes the versions of the Nuke script are in one folder
@@ -583,7 +663,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
             regex_valid = True
         except re.error:
             regex_valid = False
-            print("Regex Error")
+            logger.error("Version Regex Error")
             return
 
         if self.found_versions is not None:
@@ -597,12 +677,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
                         try:
                             # if user made a group
                             version = m.group(1)
+                            tokens = self.get_script_path_tags(one_path + '/' + one_file)
+                            t = ''
+                            for k, v in tokens.items():
+                                t += f"{k}:{v};"
+
                             my_dict = {
                                 'path': one_path,
                                 'file': one_file,
                                 'version': version,
                                 'full': one_path + '/' + one_file,
-                                'apply': True
+                                'apply': True,
+                                'tokens': t
                             }
                             one_folder.append(my_dict)
                         except:
@@ -646,6 +732,62 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
             self.filtered_versions[row]['apply'] = True
         #self.display_table()
 
+    def my_time(self):
+        """
+        Used for timestamping packages
+        Stores UTC time, and adds UTC offset for user comfort
+        Offset assumes plus sign, unless - present
+        """
+        # get utc time YYMMDD-HHMM_
+        utc = datetime.datetime.now(datetime.timezone.utc).strftime('%y%m%d_%H%M_')
+        # get utc to local time offset, this is just for user
+        offset = datetime.datetime.now(datetime.timezone.utc).astimezone().strftime('%z').replace('+', '')
+        return utc + offset
+
+    def _fill_job_path(self):
+        class Default(dict):
+            def __missing__(self, key):
+                return key
+
+        if self.settings_json is None or self.settings_json == {}:
+            return None
+        if self.place_list is None or self.place_list == []:
+            return None
+        place_source = self.ui.place_source.currentText()
+        place_target = self.ui.place_target.currentText()
+
+        self.settings_json['places'][place_source]['is_source'] = True
+        self.settings_json['places'][place_source]['is_target'] = False
+        self.settings_json['places'][place_target]['is_source'] = False
+        self.settings_json['places'][place_target]['is_target'] = True
+
+        self.anatomy = {
+            'timestamp': self.my_time(),
+            'place_source': place_source,
+            'place_target': place_target,
+        }
+        self.anatomy.update(self.settings_json['places'][place_source]['anatomy'])
+        # job root
+        self.ui.job_folder.setText(self.settings_json['job']['job_root'].format_map(Default(self.anatomy)).replace("\\", "/"))
+        # job folder
+        self.ui.job_name.setText(self.settings_json['job']['job_name_default'].format_map(Default(self.anatomy)).replace("\\", "/"))
+
+    def _validate_job_name(self):
+        if not re.match(self.ui.job_template.displayText(), self.ui.job_name.displayText()):
+            logger.error("Make sure job name matches convention.")
+            return False
+        else:
+            logger.info("Job name matches convention.")
+            return True
+
+    def place_change(self):
+        place_source = self.ui.place_source.currentText()
+        place_target = self.ui.place_target.currentText()
+        if place_source == place_target:
+            logging.error("Source and target places have to be different")
+        else:
+            self._fill_job_path()
+
     def display_json(self):
 
         self.settings_file = str(self.ui.settings_file.displayText()).replace('\\', '/')
@@ -658,6 +800,42 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
                 self.ui.settings.show()
                 self.ui.settings.header().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
                 #self.ui.settings.setAlternatingRowColors(True)
+
+        if self.settings_json is not None and self.settings_json != {}:
+            self.ui.job_template.setText(self.settings_json['job']['job_name_check'])
+            self.ui.job_template.setText(self.settings_json['job']['job_name_check'])
+
+            self.place_list = list(self.settings_json['places'].keys())
+            if len(self.place_list) >= 2:
+                self.ui.place_source.blockSignals(True)
+                self.ui.place_source.clear()
+                self.ui.place_source.addItems(self.place_list)
+                self.ui.place_target.blockSignals(True)
+                self.ui.place_target.clear()
+                self.ui.place_target.addItems(self.place_list)
+                try:
+                    preferred_source = self.ui.place_source_default.displayText()
+                    if preferred_source in self.place_list:
+                        i = self.place_list.index(preferred_source)
+                        self.ui.place_source.setCurrentIndex(i)
+                        # set target to close place that is not source
+                        if i == 0:
+                            self.ui.place_target.setCurrentIndex(1)
+                        else:
+                            self.ui.place_target.setCurrentIndex(0)
+                    else:
+                        self.ui.place_source.setCurrentIndex(0)
+                        self.ui.place_target.setCurrentIndex(1)
+                except:
+                    logger.error("Failed to set places dropbox")
+
+                self.ui.place_source.blockSignals(False)
+                self.ui.place_target.blockSignals(False)
+
+                self._fill_job_path()
+            else:
+                logger.error(f"Found {len(self.place_list)} places defined in settings json. At least two are required.")
+
 
     def display_table(self):
 
@@ -699,6 +877,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
                         self.ui.tableWidget.setItem(row, column_number,
                                                     QtWidgets.QTableWidgetItem(
                                                         str(line['full'])))
+                    elif my_title == 'tokens':
+                        self.ui.tableWidget.setItem(row, column_number,
+                                                    QtWidgets.QTableWidgetItem(
+                                                        str(line['tokens'])))
                     else:
                         logging.error("Can't display table row")
 
@@ -724,66 +906,47 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
     def apply(self, nuke_file):
         pass
 
-
     def go(self):
+        def make_dir(dir):
+            try:
+                os.makedirs(dir)
+            except OSError:
+                pass
+            return os.path.isdir(dir)
 
+        _job_path = self.ui.job_folder.displayText() + '/' + self.ui.job_name.displayText()
+        self.settings_json['job']['path'] = _job_path
+        self.settings_json['job']['name'] = self.ui.job_name.displayText()
+
+        r = make_dir(_job_path)
+        if not r:
+            logging.error(f"Can't create directory {_job_path}")
+        pack_dir = _job_path + '/_pack_nuke'
+        r = make_dir(pack_dir)
+        if not r:
+            logging.error(f"Can't create directory {pack_dir}")
+        _csv = pack_dir + '/nuke_files.csv'
+        titles = ['Name', 'Full Path', 'Tokens', 'Package', 'Source', 'Target']
         total = 0
-        files = []
-        for one in self.filtered_versions:
-            if one['apply']:
-                total += 1
-                files.append(one['full'])
-        total_versions = len(self.filtered_versions)
+        with open(_csv, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(titles)
+            for one in self.filtered_versions:
+                if one['apply']:
+                    one_row = [
+                        one['file'],
+                        one['full'],
+                        one['tokens'],
+                        self.ui.job_name.displayText(),
+                        self.ui.place_source.currentText(),
+                        self.ui.place_target.currentText(),
+                    ]
+                    writer.writerow(one_row)
+                    total += 1
 
-        _go = False
-        button = QtWidgets.QMessageBox.question(
-            self, "Do or don't, there is no try!",
-            "Are you sure to apply python script to {} Nuke scripts?".format(total))
-        if button == QtWidgets.QMessageBox.Yes:
-            _go = True
-        if _go:
-            logging.info('Processing started at: ' + time.strftime("%Y-%m-%d, %H:%M"))
-            cnt = 1
-            go_time = time.time()
-            self.ui.execute.setText('Processing ' + str(total) + ' scripts')
-            self.ui.execute.setEnabled(False)
-            success = []
-            for row, line in enumerate(self.filtered_versions):
-                QApplication.processEvents()
-                if self.user_stopped:
-                    print("Breaking")
-                    break
-
-                if bool(line['apply']):
-                    start_time = time.time()
-                    self.ui.tableWidget.item(row, 0).setBackground(QtGui.QColor('darkgreen'))
-                    self.ui.tableWidget.repaint()
-                    logging.info(line['full'])
-
-                    # apply
-                    apply_ok = self.apply(line['full'])
-                    if not apply_ok:
-                        logging.error('Error processing file: {}'.format(line['full']))
-                    else:
-                        success.append(os.path.basename(line['full']) + '\n')
-
-                    last_apply_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
-                    elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - go_time))
-                    average = float(time.time() - go_time) / float(cnt)
-                    time_needed = time.strftime("%H:%M:%S", time.gmtime((total - cnt) * average))
-                    status = "Just Processed file {}/{}: {}.Last apply time: {}, elapsed time: {}, estimated time: {}".format(cnt, total, line['file'], last_apply_time, elapsed_time, time_needed)
-                    self.ui.execute.setText(status)
-                    logging.info(status)
-                    cnt += 1
-
-                    # uncheck apply for item that is done
-                    self.handle_apply(row, 0)
-
-            # Change back to ready
-            logging.info('Processed files: \n{}'.format(''.join(success)))
-
-            self.ui.execute.setText('Execute')
-            self.ui.execute.setEnabled(True)
+        _set = pack_dir + '/settings.json'
+        with open(_set, 'w') as json_file:
+            json.dump(self.settings_json, json_file)
 
     def user_stop(self):
         QApplication.processEvents()
@@ -792,7 +955,35 @@ class MainWindow(QtWidgets.QMainWindow, Ui_pack_gui):
         QApplication.processEvents()
 
 
+def get_app_path():
+    application_path = None
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app
+        # path into variable _MEIPASS'.
+        #self.application_path = sys._MEIPASS.replace('\\', '/')
+        application_path = os.path.dirname(sys.executable).replace('\\', '/')
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
+    return application_path
+
+
 if __name__ == "__main__":
+
+    # log
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter_time = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+    formatter = logging.Formatter('%(levelname)s:%(message)s')
+    app_path = get_app_path()
+    if app_path is not None:
+        app_path += '/pack_nuke_gui.log'
+        file_handler = logging.FileHandler(app_path)
+        logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle("Plastique")
     app = app.instance()
